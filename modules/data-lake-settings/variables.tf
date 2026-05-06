0b801d98840488516214b686bdf1a807132246f5
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-05-06 18:12:44 +0000

    feat: update variable types and descriptions in data lake settings module
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- modules/data-lake-settings/variables.tf
+++ modules/data-lake-settings/variables.tf
@@ -1,18 +1,22 @@
 variable "admins" {
-  type = list(string)
+  description = "Set of ARNs of AWS Lake Formation principals"
+  type        = set(string)
 }
 
 variable "read_only_admins" {
-  type    = list(string)
-  default = []
+  description = "Set of ARNs of AWS Lake Formation principals with only view access to the resources"
+  type        = set(string)
+  default     = []
 }
 
 variable "trusted_resource_owners" {
-  type    = list(string)
-  default = []
+  description = "List of the resource-owning account IDs that the caller's account can use to share their user access details"
+  type        = set(string)
+  default     = []
 }
 
 variable "cross_account_version" {
-  type    = string
-  default = "5"
+  description = "Version of cross-account permissions, see https://docs.aws.amazon.com/lake-formation/latest/dg/optimize-ram.html"
+  type        = string
+  default     = "5"
 }
