6f28ef7e92111a041b0037da9a6a59cfa9d2bc96
Jacob Woffenden <jacob.woffenden@justice.gov.uk>
2026-05-06 19:18:58 +0000

    feat: add installation steps for terraform-docs in post-create script
    Signed-off-by: Jacob Woffenden <jacob.woffenden@justice.gov.uk>

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -4,3 +4,13 @@ set -euo pipefail
 
 # Install pre-commit hooks
 uvx pre-commit install
+
+# Install terraform-docs
+curl --location "https://github.com/terraform-docs/terraform-docs/releases/download/v0.23.0/terraform-docs-v0.23.0-linux-arm64.tar.gz" \
+  --output /tmp/terraform-docs.tar.gz
+
+tar --extract --gzip --file /tmp/terraform-docs.tar.gz --directory /tmp/ terraform-docs
+
+sudo install --owner=vscode --group=vscode --mode=775 "/tmp/terraform-docs" "/usr/local/bin/terraform-docs"
+
+rm --recursive --force /tmp/terraform-docs /tmp/terraform-docs.tar.gz
